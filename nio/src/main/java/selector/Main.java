296c8baf50ab9d63b6b4bfee9fa9efedb215fe8a
zhangshihai1232 <zhangshihai1232@126.com>
2016-10-18 22:18:58 +0800

    补充提交

---
+++ nio/src/main/java/selector/Main.java
@@ -0,0 +1,13 @@
+package selector;
+
+import java.io.IOException;
+import java.nio.channels.Selector;
+
+/**
+ * Created by zsh on 2016/10/12.
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Selector selector = Selector.open();
+    }
+}
